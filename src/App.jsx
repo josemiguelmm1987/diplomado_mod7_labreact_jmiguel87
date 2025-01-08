ca52103ee1d14ca052bd4c38243097bbe942b785
JOSE MIGUEL MENDEZ MELENA <josemiguelmm1987@gmail.com>
2025-01-07 21:32:22 -0400

    Agregando nombre de Diplomado y Módulo

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,9 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <h2>
+        Diplomado en FullStack Developer Backend and Frontend, Módulo 7.
+      </h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}

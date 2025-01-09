862eed7845bb9c9fa458120cfc0fda47a948fe75
JOSE MIGUEL MENDEZ MELENA <josemiguelmm1987@gmail.com>
2025-01-08 21:04:43 -0400

    Lab insertando logo

---
+++ src/screens/Home.jsx
@@ -0,0 +1,15 @@
+import Logo from "../Components/Home/Logo.jsx";
+import Body from "../Components/Home/Body.jsx";
+import Footer from "../Components/Home/Footer.jsx";
+
+const Home = () => {
+  return (
+    <div>
+      <Logo></Logo>
+      <Body></Body>
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default Home;

c94c14ae2aee35c0c1f170a53680c67fcc6bf92e
JOSE MIGUEL MENDEZ MELENA <josemiguelmm1987@gmail.com>
2025-01-08 20:50:10 -0400

    Implementando Componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import './App.css'
-import Default from './Components/Default.jsx'
+import Logo from './Components/Home/Logo.jsx'
+import Body from './Components/Home/Body.jsx'
+import Footer from './Components/Home/Footer.jsx'
 
 function App() {
 
   return (
     <>
-      <Default />
+      <Logo></Logo>
+      <Body></Body>
+      <Footer></Footer>
     </>
   )
 }
---
+++ src/Components/Home/Body.jsx
@@ -0,0 +1,14 @@
+const Body = () => {
+  return (
+    <div>
+      <h2>Diplomado en FullStack Developer Backend and Frontend, Módulo 7.</h2>
+      <div className="card">
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Body;

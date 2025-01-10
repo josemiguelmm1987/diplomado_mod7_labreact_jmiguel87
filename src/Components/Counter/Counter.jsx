fbf188eeae650f6718531a825336f9d364aa7b96
JOSE MIGUEL MENDEZ MELENA <josemiguelmm1987@gmail.com>
2025-01-09 22:39:06 -0400

    Implementando incrementByAmount

--- src/Components/Counter/Counter.jsx
+++ src/Components/Counter/Counter.jsx
@@ -1,7 +1,7 @@
 // import React from 'react'
 import { useSelector, useDispatch } from "react-redux";
 // import { decrement, increment } from "./counterSlice";
-import { decrement, increment } from "../../store/features/counter/counterSlice.js";
+import { decrement, increment, incrementByAmount } from "../../store/features/counter/counterSlice.js";
 
 const Counter = () => {
   const count = useSelector((state) => state.counter.value);
@@ -24,6 +24,11 @@ const Counter = () => {
           onClick={() => dispatch(decrement())}
         >
           Decrement
+        </button> <button
+          aria-label="Increment by Amount value"
+          onClick={() => dispatch(incrementByAmount(20))}
+        >
+          Increment by Amount
         </button>
       </div>
     </div>
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -6,19 +6,23 @@ import reactLogo from "../assets/react.svg";
 import viteLogo from "../assets/vite.svg";
 import githubLogo from "../assets/github.svg";
 
-import { useSelector, useDispatch } from "react-redux";
-// import { decrement, increment } from "../../store/features/counter/counterSlice.js";
+import { useSelector } from "react-redux";
 
 const Home = () => {
   const count = useSelector((state) => state.counter.value);
-  // const dispatch = useDispatch();
   return (
     <div>
       <span>{count}</span>
       <br />
-      <OpenLink redirectUrl="https://react.dev" logoImage={reactLogo}></OpenLink>
+      <OpenLink
+        redirectUrl="https://react.dev"
+        logoImage={reactLogo}
+      ></OpenLink>
       <OpenLink redirectUrl="https://vite.dev" logoImage={viteLogo}></OpenLink>
-      <OpenLink redirectUrl="https://github.com/josemiguelmm1987/diplomado_mod7_labreact_jmiguel87.git" logoImage={githubLogo}></OpenLink>
+      <OpenLink
+        redirectUrl="https://github.com/josemiguelmm1987/diplomado_mod7_labreact_jmiguel87.git"
+        logoImage={githubLogo}
+      ></OpenLink>
       <Body></Body>
       <Footer></Footer>
     </div>

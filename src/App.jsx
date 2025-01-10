6b92777cfa9b70e539a9922fa3f97676e722ddd7
JOSE MIGUEL MENDEZ MELENA <josemiguelmm1987@gmail.com>
2025-01-09 21:19:45 -0400

    Implementando Redux State

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Home from "./screens/Home.jsx";
+import Counter from './Components/Counter/Counter.jsx'
 
 function App() {
   return (
     <>
       <Home></Home>
+      <Counter></Counter>
     </>
   );
 }
